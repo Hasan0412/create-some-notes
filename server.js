b1b529c1dbeef6d58ad0b4785cb53559d9254b3b
Hasan Malik <hmalik0412@gmail.com>
2023-08-23 07:39:34 +0100

    created server.js and imported modules

---
+++ server.js
@@ -0,0 +1,7 @@
+// Import necessary modules
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000; // Set the port
